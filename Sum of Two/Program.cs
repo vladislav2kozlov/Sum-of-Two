15c4ef3c1748b97e5daf16514e60bb3dcd36ebb4
Vlad Kozlov <Vlad Kozlov>
2017-03-25 10:48:50 -0700

    Add project files.

---
+++ Sum of Two/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sum_of_Two
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            System.Console.WriteLine(15 + 7);
+            Console.ReadLine();
+        }
+    }
+}
